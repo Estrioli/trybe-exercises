bd8d3cfbdc8efcf094f8074690d351590204aca9
Rafael <estrioli@gmail.com>
2022-09-21 15:46:51 -0300

    Para fixar :rocket:

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const myName = 'Rafael';
+const birthCity = 'Indaiatuba';
+let birthYear = 1990;
+birthYear = 2030;
+birthCity = 'SP';
+
+console.log(birthCity)
+
+/*O erro está em querer alterar uma CONST
+essa variavel nao pode ser alterada.
