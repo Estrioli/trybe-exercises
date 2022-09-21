c215714094bf35c991f12d51db25ba88a078cd0f
Rafael <estrioli@gmail.com>
2022-09-21 20:00:33 -0300

    :rocket:exercicio triangulo

---
+++ exercicios.js
@@ -0,0 +1,13 @@
+const a = 60;
+const b = 50;
+const c = -70;
+
+if(a+b+c == 180) {
+console.log(true)
+
+}else if(a+b+c !== 180 && a, b, c > 0){
+console.log(false)
+
+}else{
+    console.log('inválido')
+}
