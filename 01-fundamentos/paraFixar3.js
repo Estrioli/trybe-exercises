0f48ad85c9f90b39529e19eb334d98ff3feac696
Rafael <estrioli@gmail.com>
2022-09-21 23:44:00 -0300

    paraFixar operadores logicos de comparação

---
+++ 01-fundamentos/paraFixar3.js
@@ -0,0 +1,11 @@
+let weekDay = 'sabado';
+
+if (weekDay == 'segunda-feira'||weekDay == 'terca-feira'|| weekDay == 'quarta-feira'|| weekDay == 'quinta-feira'||weekDay == 'sexta-feira') {
+    console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+
+}else if (weekDay == 'sabado'||weekDay == 'domingo') {
+    console.log('FINALMENTE, descanso merecido UwU');
+
+}else {
+    console.log('insira o dia correto');
+}
