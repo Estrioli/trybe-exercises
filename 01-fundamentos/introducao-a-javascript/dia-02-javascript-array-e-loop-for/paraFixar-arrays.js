982c2ada6e8ff60c43a40ebcf75598b30b1c754d
Rafael <estrioli@gmail.com>
2022-09-22 16:06:29 -0300

    fix exercises - arrays

---
+++ 01-fundamentos/introducao-a-javascript/dia-02-javascript-array-e-loop-for/paraFixar-arrays.js
@@ -0,0 +1,16 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu[2] ;
+
+console.log(indexOfPortfolio);
+
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('contato');
+
+console.log(menu);
