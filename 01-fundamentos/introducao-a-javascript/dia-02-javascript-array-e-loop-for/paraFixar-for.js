511077a1e91ec69862d3d3e6997f5714af45d37c
Rafael <estrioli@gmail.com>
2022-09-22 17:19:51 -0300

    fix exercises - for

---
+++ 01-fundamentos/introducao-a-javascript/dia-02-javascript-array-e-loop-for/paraFixar-for.js
@@ -0,0 +1,14 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index+=1) {
+    
+} console.log(groceryList);
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let index = 0; index < names.length; index+=1) {
+    
+}
+console.log(names)
+
