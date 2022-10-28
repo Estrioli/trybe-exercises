7f144b5b7d8ed385113c3b05701efdc70a374e31
Rafael <estrioli@gmail.com>
2022-10-27 21:08:03 -0300

    exercise 2

---
+++ 01-fundamentos/higher-order-functions/dia-04-reduce/exercise2.js
@@ -0,0 +1,82 @@
+const books = [
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: "A Coisa",
+    genre: "Terror",
+    author: {
+      name: "Stephen King",
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: {
+      name: "H. P. Lovecraft",
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+//Crie uma string com os nomes de todas as pessoas autoras.
+const reduceNames = (books) =>
+  books.reduce((acc, cur) => `${acc} ${cur.name}, `, "");
+console.log(reduceNames(books));
+
+//Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+const averageAge = (books) =>
+  books.reduce((acc, cur) => acc + (cur.releaseYear - cur.author.birthYear), 0)/books.length
+
+console.log(averageAge(books));
+
+//Encontre o livro com o maior nome.
+const longestNamedBook = (books) => books.reduce((laterBook, nextBook) => {
+    if (laterBook.name.length > nextBook.name.length) return laterBook
+    return nextBook
+
+})
+console.log(longestNamedBook(books));
+
